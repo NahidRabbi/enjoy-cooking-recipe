d4668fc3b9c1907beace79c5d2fe8a7321b242e4
nahid <nahidrabbi045@gmail.com>
2024-03-19 22:11:38 +0600

    added file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,2 @@
-import './App.css'
-import Navbar from './Navbar/Navbar'
-import './index.css'
-import Recepies from './recepies/Recepies'
-import Footer from './footer/Footer'
 
-function App() {
-
-
-  return (
-    <>
-     
-     <Navbar></Navbar>
-     <Recepies></Recepies>
-     <Footer></Footer>
-     
-      
-    </>
-  )
-}
-
-export default App
 
